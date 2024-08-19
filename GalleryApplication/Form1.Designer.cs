2f61fec0a16fd0c93472f821482dd26d88691bce
begum-aydinatay <132909653+begum-aydinatay@users.noreply.github.com>
2024-08-19 08:32:12 +0200

    Gallery screen added, 'add' functionality is added.

--- GalleryApplication/Form1.Designer.cs
+++ GalleryApplication/Form1.Designer.cs
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip1 = new MenuStrip();
+            menuFile = new ToolStripMenuItem();
+            menuOpen = new ToolStripMenuItem();
+            menuSave = new ToolStripMenuItem();
+            toolStripMenuItem1 = new ToolStripSeparator();
+            menuAdd = new ToolStripMenuItem();
+            toolStripMenuItem2 = new ToolStripSeparator();
+            menuExit = new ToolStripMenuItem();
+            flowLayoutPanel = new FlowLayoutPanel();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = SystemColors.Control;
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { menuFile });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Padding = new Padding(8, 2, 0, 2);
+            menuStrip1.Size = new Size(757, 29);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // menuFile
+            // 
+            menuFile.BackColor = SystemColors.Control;
+            menuFile.DropDownItems.AddRange(new ToolStripItem[] { menuOpen, menuSave, toolStripMenuItem1, menuAdd, toolStripMenuItem2, menuExit });
+            menuFile.Font = new Font("Tahoma", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            menuFile.ForeColor = SystemColors.ActiveCaptionText;
+            menuFile.Name = "menuFile";
+            menuFile.Size = new Size(50, 25);
+            menuFile.Text = "File";
+            // 
+            // menuOpen
+            // 
+            menuOpen.Name = "menuOpen";
+            menuOpen.Size = new Size(133, 26);
+            menuOpen.Text = "Open";
+            // 
+            // menuSave
+            // 
+            menuSave.Name = "menuSave";
+            menuSave.Size = new Size(133, 26);
+            menuSave.Text = "Save";
+            // 
+            // toolStripMenuItem1
+            // 
+            toolStripMenuItem1.Name = "toolStripMenuItem1";
+            toolStripMenuItem1.Size = new Size(130, 6);
+            // 
+            // menuAdd
+            // 
+            menuAdd.Name = "menuAdd";
+            menuAdd.Size = new Size(133, 26);
+            menuAdd.Text = "Add";
+            menuAdd.Click += menuAdd_Click;
+            // 
+            // toolStripMenuItem2
+            // 
+            toolStripMenuItem2.Name = "toolStripMenuItem2";
+            toolStripMenuItem2.Size = new Size(130, 6);
+            // 
+            // menuExit
+            // 
+            menuExit.Name = "menuExit";
+            menuExit.Size = new Size(133, 26);
+            menuExit.Text = "Exit";
+            // 
+            // flowLayoutPanel
+            // 
+            flowLayoutPanel.BackColor = SystemColors.ControlLight;
+            flowLayoutPanel.Dock = DockStyle.Fill;
+            flowLayoutPanel.Location = new Point(0, 29);
+            flowLayoutPanel.Margin = new Padding(4, 3, 4, 3);
+            flowLayoutPanel.Name = "flowLayoutPanel";
+            flowLayoutPanel.Padding = new Padding(15);
+            flowLayoutPanel.Size = new Size(757, 432);
+            flowLayoutPanel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(757, 461);
+            Controls.Add(flowLayoutPanel);
+            Controls.Add(menuStrip1);
+            Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ForeColor = SystemColors.Control;
+            MainMenuStrip = menuStrip1;
+            Margin = new Padding(4, 3, 4, 3);
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuFile;
+        private ToolStripMenuItem menuOpen;
+        private ToolStripMenuItem menuSave;
+        private ToolStripSeparator toolStripMenuItem1;
+        private ToolStripMenuItem menuAdd;
+        private ToolStripSeparator toolStripMenuItem2;
+        private ToolStripMenuItem menuExit;
+        private FlowLayoutPanel flowLayoutPanel;
     }
 }
--- GalleryApplication/Form1.cs
+++ GalleryApplication/Form1.cs
@@ -6,5 +6,38 @@ namespace GalleryApplication
         {
             InitializeComponent();
         }
+
+        private void menuAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JPEG Images(*.jpg;*.jpeg)|*.jpg;*.jpeg|PNG Images(*.png)|*.png|All Files(*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+            ofd.FilterIndex = 3;
+            ofd.Title = "Select Images";
+            ofd.Multiselect = true;
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                string[] filePaths = ofd.FileNames;
+
+                foreach (string p in filePaths)
+                {
+                    Panel pnl = new Panel();
+                    pnl.BorderStyle = BorderStyle.FixedSingle;
+                    pnl.Height = 75;
+                    pnl.Width = 75;
+
+                    PictureBox pb = new PictureBox();
+                    pb.SizeMode = PictureBoxSizeMode.Zoom;
+                    pb.ImageLocation = p;
+                    pb.Dock = DockStyle.Fill;   
+
+                    pnl.Controls.Add(pb);
+
+                    flowLayoutPanel.Controls.Add(pnl);
+                }
+
+            }
+        }
     }
 }
